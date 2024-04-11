ff21f3746858101cbef1b77d463d6d67935910c5
coreysawin <sawincp@gmail.com>
2024-04-11 14:59:07 -0400

    update app controller, login comp and Public folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,34 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-
-    SECRETE_KEY = Rails.application.secrete_key_base
-
+  
+    SECRET_KEY = Rails.application.secrets.secret_key_base  # Use secrets.secret_key_base for security
+  
     def encode_token(payload)
-        JWT.encode(payload, SECRETE_KEY)
+      JWT.encode(payload, SECRET_KEY, algorithm: 'HS256')  # Explicitly specify algorithm
     end
-
+  
     def session_user
-        decoded_hash = decoded_token
-        if !decoded_hash.empty? 
-            puts decoded_hash.class
-            user_id = decoded_hash[0]['user_id']
-            @user = User.find_by(id: user_id)
-        else
-            nil 
-        end
+      decoded_hash = decoded_token
+      return nil if decoded_hash.empty?  # Early return if empty
+  
+      user_id = decoded_hash[0]['user_id']
+      @user = User.find_by(id: user_id)
     end
-
+  
     def auth_header
-        request.headers['Authorization']
+      request.headers['Authorization']
     end
-
-    def decode_token
-        if auth_header
-            token = auth_header.split(' ')[1]
-            begin
-                JWT.decode(token, SECRETE_KEY, true, algorithim 'HS256')
-            rescue JWT::DecodeError
-                []
+  
+    def decoded_token
+      if auth_header
+        token = auth_header.split(' ')[1]
+        begin
+          JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')
+        rescue JWT::DecodeError
+          return []  # Return empty array on decode error
         end
+      else
+        return []  # Return empty array if no auth header
+      end
     end
-end
+  end
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,36 +1,29 @@
 import React,{ useState } from 'react'
+import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
 import LoginForm from './LoginForm'
 import SignupForm from './SignupForm'
 
 function Login() {
-    const [showLogin, setShowLogin]= useState(true)
+  const [showLogin, setShowLogin] = useState(true);
 
   return (
-    <div>
-    {showLogin ? (
-      <>
-      <LoginForm  />
-      <p>
-        Don't have an account?
-      </p>
-      <Button onClick ={()=> setShowLogin(false)}>
-          Sign Up!
-        </Button>
-      </>
-    ) : (
-      <>
-      <SignupForm />
-      <p>
-        Already have an account?
-      </p>
-      <Button onClick={()=> setShowLogin(true)}>
-          Log In!
-        </Button>
-      </>
-    )}
-  </div>
-  )
+    <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      {showLogin ? (
+        <>
+          <LoginForm />
+          <p>Don't have an account?</p>
+          <Button onClick={() => setShowLogin(false)}>Sign Up!</Button>
+        </>
+      ) : (
+        <>
+          <SignupForm />
+          <p>Already have an account?</p>
+          <Button onClick={() => setShowLogin(true)}>Log In!</Button>
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
