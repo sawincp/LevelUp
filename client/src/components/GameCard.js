7369ee12125456906fdbf3a7548828b34cedf3e2
coreysawin <sawincp@gmail.com>
2024-04-18 17:07:34 -0400

    update flip annimation

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -5,21 +5,27 @@ import Col from 'react-bootstrap/esm/Col';
 import { useSpring, animated } from '@react-spring/web';
 
 const GameCard = ({ games }) => {
-  const [flippedCards, setFlippedCards] = useState({}); // Object to track flipped states
+  const [flipped, setFlipped] = useState(false);
 
   const handleClick = (gameId) => {
-    setFlippedCards((prevFlippedCards) => ({ ...prevFlippedCards, [gameId]: !prevFlippedCards[gameId] })); // Toggle flip state for clicked card
+    setFlipped((prevState) => !prevState); // Toggle flipped state on click
   };
+
+  const { transform, opacity } = useSpring({
+    from: { opacity: 1, transform: 'perspective(600px) rotateX(0deg)' }, // Initial state
+    to: { opacity: flipped ? 1 : 1, transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)` }, // Flipped state
+    config: { mass: 5, tension: 500, friction: 80 }, // Spring configuration
+  });
 
   return (
     <Row xs={1} md={2} className="g-4">
       {games.map((game) => (
         <Col key={game.id}>
-          <animated.div onClick={() => handleClick(game.id)} style={{ transform: flippedCards[game.id] ? 'perspective(600px) rotateX(180deg)' : 'perspective(600px) rotateX(0deg)', opacity: flippedCards[game.id] ? 1 : 1 ,  config: { mass: 5, tension: 500, friction: 80 }}}>
+          <animated.div onClick={() => handleClick(game.id)} style={{ transform, opacity }}>
             <Card>
               <Card.Img variant="top" src={game.cover_art} className="img-thumbnail" style={{ width: '300px', height: '200px', objectFit: 'cover' }} />
               <Card.Body>
-                {flippedCards[game.id] ? (
+                {flipped ? (
                   // Content for the back of the card
                   <div>
                     <h2>Back of {game.title}</h2>
@@ -43,3 +49,4 @@ const GameCard = ({ games }) => {
 };
 
 export default GameCard;
+
